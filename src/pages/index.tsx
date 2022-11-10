c04ad69d16e423f19f2d10a28a7c9a63c2caa1a4
Luis Paulo Ruda <luispauloruda@gmail.com>
2022-11-09 23:36:22 -0300

    feat: home structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,31 @@
+import Image from "next/image";
+
+import shirt1 from '../assets/shirts/1.png';
+import shirt2 from '../assets/shirts/2.png';
+import shirt3 from '../assets/shirts/3.png';
+import shirt4 from '../assets/shirts/4.png';
+import { HomeContainer, Product } from "../styles/pages/home";
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <HomeContainer>
+      <Product>
+        <Image src={shirt1} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={shirt2} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
