16fd108dd0cc339b3aa327c916f00a2b9a85ad62
Luis Paulo Ruda <luispauloruda@gmail.com>
2022-11-12 23:18:20 -0300

    feat: convert SSR to SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 import Image from "next/image";
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import { useKeenSlider } from 'keen-slider/react';
 
 import 'keen-slider/keen-slider.min.css';
@@ -42,7 +42,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price'],
   });
@@ -61,6 +61,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       products
-    }
+    },
+    revalidate: 60 * 60 * 2, //2 hours
   }
 }
