ad9127f2f2d300042a36824cd4bf2d703864bd23
Luis Paulo Ruda <luispauloruda@gmail.com>
2022-12-22 16:53:51 -0300

    feat: product page structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,25 @@
+
 import { useRouter } from "next/router"
+
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer />
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime necessitatibus, at doloremque quo pariatur aliquid expedita provident sunt reiciendis mollitia perferendis, dolorum neque cupiditate architecto quae officiis, eos itaque voluptatem?</p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
