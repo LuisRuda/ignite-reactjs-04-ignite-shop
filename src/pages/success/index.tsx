635f31f733f7f180fb4f5e995dfe28b72b895980
Luis Paulo Ruda <luispauloruda@gmail.com>
2023-02-27 11:21:59 -0300

    feat: success page structure

--- src/pages/success/index.tsx
+++ src/pages/success/index.tsx
@@ -1,5 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+import { SuccessContainer, ImageContainer } from "../../styles/pages/success";
+
 export default function Success() {
   return (
-    <h1>Success</h1>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>
+        {/* <Image /> */}
+      </ImageContainer>
+
+      <p>Uhull <strong>Luis</strong>, sua camiseta <strong>teste</strong> já está a caminho da sua casa.</p>
+
+      <Link href="/">Voltar ao catalogo</Link>
+    </SuccessContainer>
   )
 }
