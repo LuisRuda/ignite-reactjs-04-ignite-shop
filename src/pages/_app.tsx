b171b7feccd7be427913f21cee2e1ae76a006502
Luis Paulo Ruda <luispauloruda@gmail.com>
2023-03-06 21:38:28 -0300

    feat: created cart button in header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,9 @@ import Image from 'next/image'
 import type { AppProps } from 'next/app'
 
 import logoImg from '../assets/logo.svg'
+import cartImg from '../assets/cart.svg'
 import { globalStyles } from '../styles/global'
-import { Container, Header } from '../styles/pages/app'
+import { Container, Header, CartButton } from '../styles/pages/app'
 
 globalStyles()
 
@@ -11,7 +12,13 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <Image src={logoImg} alt="" />
+        <Image src={logoImg} alt="Ignite Shop" />
+        <CartButton>
+          <div>
+            <span className="cart-counter">1</span>
+          </div>
+          <Image src={cartImg} alt="Carrinho" />
+        </CartButton>
       </Header>
 
       <Component {...pageProps} />
--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -11,6 +11,45 @@ export const Container = styled('div', {
 export const Header = styled('header', {
   padding: '2rem 0',
   width: '100%',
-  maxWidth: 1100,
+  maxWidth: 1200,
   margin: '0 auto',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+})
+
+export const CartButton = styled('button', {
+  width: '3rem',
+  height: '3rem',
+  border: 'none',
+  cursor: 'pointer',
+  borderRadius: '6px',
+  position: 'relative',
+  backgroundColor: '$gray800',
+
+  div: {
+    width: '1.9rem', 
+    height: '1.9rem',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: '-10px',
+    right: '-10px',
+    borderRadius: '50%',
+    backgroundColor: '$gray900',
+
+    span: {
+      width: '1.5rem', 
+      height: '1.5rem',
+      color: '$white',
+      backgroundColor: '$green500',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: '50%'
+    }
+  }
+
+  
 })
